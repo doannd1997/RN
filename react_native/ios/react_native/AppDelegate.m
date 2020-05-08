929b642a9517fbaf87828743e6432e9971376696
doannd1997 <duydoan0907@gmail.com>
2020-05-08 22:26:56 +0700

    Configure msps ios

--- react_native/ios/react_native/AppDelegate.m
+++ react_native/ios/react_native/AppDelegate.m
@@ -11,10 +11,14 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyCcuUK8Q8drVszgClwhfIVMXImXazFVaGE"];
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"react_native"
