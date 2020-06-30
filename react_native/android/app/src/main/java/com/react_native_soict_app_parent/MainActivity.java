4e669668a614071e6e547af372d5928d7411d4f6
doannd1997 <duydoan0907@gmail.com>
2020-06-30 07:48:31 +0700

    Change Package Name to "com.react_native_soict_app_parent"

--- react_native/android/app/src/main/java/com/react_native_soict_app_parent/MainActivity.java
+++ react_native/android/app/src/main/java/com/react_native_soict_app_parent/MainActivity.java
@@ -1,4 +1,4 @@
-package com.react_native;
+package com.react_native_soict_app_parent;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "react_native";
+    return "react_native_soict_app_parent";
   }
 }
